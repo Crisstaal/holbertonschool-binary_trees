bdeea02bb1c6708fd9a62a7aa76b3cfe284f98cb
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2023-12-22 10:27:43 +0000

    left and right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - insert new node at the right
+ * @parent: node
+ * @value: value
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node, *tmp;
+
+	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
+	if (parent->right)
+	{
+		tmp = parent->right;
+		tmp->parent = node;
+		node->right = tmp;
+	}
+	node->parent = parent;
+	parent->right = node;
+	return (node);
+}
